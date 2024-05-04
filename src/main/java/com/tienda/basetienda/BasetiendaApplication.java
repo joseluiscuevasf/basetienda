da46dac328cf14f4e092aaf08769a780cc29d5ab
Jose <jcuevasf@gmail.com>
2024-05-03 20:42:28 -0600

    Se agrega base de datos tienda

---
+++ src/main/java/com/tienda/basetienda/BasetiendaApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.basetienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasetiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasetiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/basetienda/BasetiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.basetienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasetiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
